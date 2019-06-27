eb20c4c84edfc35310f76bde9c127bcb9a77c301
Lewis <lewm20@gmail.com>
2019-06-27 00:23:55 -0400

    add iexcloud and form

--- ttp_stage_2/src/Home.js
+++ ttp_stage_2/src/Home.js
@@ -1,17 +1,95 @@
 import React, { Component } from 'react';
+import { API, graphqlOperation, Auth } from 'aws-amplify';
+import { createStock, updateStock, updateUser } from './graphql/mutations';
+import { getUser, listStocks } from './graphql/queries';
 import './Home.css';
 
 class Home extends Component {
   state = {
-    amount: 5000,
-    stocks: [
-      { name: 'AAPL', amount: 6, price: 2000 },
-      { name: 'AAPL', amount: 6, price: 2000 },
-      { name: 'AAPL', amount: 6, price: 2000 },
-      { name: 'AAPL', amount: 6, price: 2000 },
-    ],
+    cash: 0,
+    stocks: [],
+    data: {},
     ticker: '',
     quantity: '',
+    id: '',
+  };
+
+  componentDidMount = async () => {
+    await this.getUserCred();
+    const stocks = await API.graphql(graphqlOperation(listStocks));
+    const user = await API.graphql(graphqlOperation(getUser, { id: this.state.id }));
+    const { cash } = user.data.getUser;
+    this.setState({ stocks: stocks.data.listStocks.items, cash });
+  };
+  getUserCred = async () => {
+    let user = await Auth.currentAuthenticatedUser();
+    this.setState({ id: user.attributes.sub });
+  };
+  getData = async () => {
+    const { ticker, quantity } = this.state;
+    const endpoint = `https://cloud.iexapis.com/stable/tops?token=pk_ecbab72853cd4c92b7ccde66561abfbf&symbols=${ticker}`;
+    const response = await fetch(endpoint);
+    let data = await response.json();
+    data = {
+      ticker: data[0].symbol,
+      price: data[0].lastSalePrice,
+      quantity,
+    };
+    // await this.getUserCred();
+    this.setState({ data });
+    this.createStock();
+  };
+
+  createStock = async () => {
+    const { id, data, cash, stocks } = this.state;
+    const { ticker, quantity, price } = data;
+    const input = {
+      ticker,
+      price: price * quantity,
+      stockUserId: id,
+      quantity: parseInt(quantity),
+    };
+    const stock = await API.graphql(graphqlOperation(getUser, { id })); //check db for stock
+    let foundStock = stock.data.getUser.stocks.items.find(item => item.ticker === ticker);
+    //update or create stock
+    if (foundStock) {
+      {
+        const { price, quantity, id } = foundStock;
+        const newPrice = input.price + price;
+        const newQuantity = input.quantity + quantity;
+        const updatedInput = {
+          id,
+          ticker,
+          quantity: newQuantity,
+          price: newPrice,
+        };
+        if (cash >= 0 && cash > (data.price * quantity)) {
+          let data = await API.graphql(graphqlOperation(updateStock, { input: updatedInput }));
+          let index = stocks.findIndex(stock => stock.id === data.data.updateStock.id);
+          const { price, quantity } = data.data.updateStock;
+          stocks[index] = { ...stocks[index], price, quantity };
+          this.setState({ stocks });
+        }
+      }
+    } else {
+      if (cash >= 0 && cash > (price * quantity)) {
+        let data = await API.graphql(graphqlOperation(createStock, { input }));
+        const stocks = [...this.state.stocks, data.data.createStock];
+        this.setState({ stocks });
+      }
+    }
+    if (cash > 0 && cash > (price * quantity)) {
+      const input = {
+        id,
+        cash: parseInt(cash - price * quantity),
+      };
+      await API.graphql(graphqlOperation(updateUser, { input: input }));
+
+    console.log(cash);
+    this.setState({
+      cash: parseInt(cash - price * quantity),
+    });
+  }
   };
 
   handleChange = event => {
@@ -22,36 +100,18 @@ class Home extends Component {
     });
   };
 
-  getData = async() => {
-    let query = this.state.ticker;
-    let endpoint = `https://cloud.iexapis.com/?filter=aapl`;
-    fetch(endpoint)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .then(responseJson => {
-        console.log(responseJson);
-        // this.addData(responseJson);
-      })
-      .catch(error => {
-        console.log(error);
-        // this.setState({ inputClass: 'animated shake search red' });
-        // setTimeout(() => this.setState({ inputClass: 'search' }), 1000);
-      });
-  };
-  handleSubmit = async event => {
+  handleSubmit = event => {
     event.preventDefault();
-    this.getData()
+    this.getData();
     this.setState({ ticker: '', quantity: '' });
   };
+
   render() {
-    const { amount, stocks } = this.state;
+    const { cash, stocks } = this.state;
     return (
       <div>
         <div className="Home-header">
-          <h1>Portfolio (${amount})</h1>
+          <h1>Portfolio (${cash})</h1>
         </div>
         <div className="Home-portfolio">
           <div className="Home-list">
@@ -59,9 +119,9 @@ class Home extends Component {
               return (
                 <ul key={id} className="row">
                   <li>
-                    {stock.name} - {stock.amount} Shares
+                    {stock.ticker} - {stock.quantity} Shares
                   </li>
-                  <li>${stock.price}</li>
+                  <li>${stock.price.toFixed(2)}</li>
                 </ul>
               );
             })}
